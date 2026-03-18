3926ab2277ebed0c916947af915b3cc869d7e3e2
josepeon <josepeon1803@gmail.com>
2026-03-17 17:00:36 -0700

    Update RSVP description with Jose Peon lookbook line

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -87,9 +87,10 @@ export default function InvitePage() {
                 marginBottom: "28px",
               }}
             >
-              Private view of the Finesse archive and lookbook shoot with
-              guests. Friday, March 20th from 3PM to 8PM. 700 S Flower St,
-              18th Floor, DTLA. Welcome cocktail, live music. Limited spots.
+              Private view of the Finesse archive. Friday, March 20th from 3PM
+              to 8PM. 700 S Flower St, 18th Floor, DTLA. Welcome cocktail,
+              live music. Jose Peon will be shooting lookbook, come ready to be
+              captured. Limited spots.
             </p>
 
             <form onSubmit={handleSubmit}>
